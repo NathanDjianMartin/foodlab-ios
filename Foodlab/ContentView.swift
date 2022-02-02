c793746628302eb212df680b67aba9dd85ef3dec
nathandjianmartin <ndmroot-git@yahoo.fr>
2022-02-02 15:15:55 +0100

    Initial Commit

---
+++ Foodlab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foodlab
+//
+//  Created by Nathan Djian Martin on 02/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foodlab/FoodlabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodlabApp.swift
+//  Foodlab
+//
+//  Created by Nathan Djian Martin on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodlabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

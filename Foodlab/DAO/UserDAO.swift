0641c221a0deda7bc5106c9fb54a563d141c29a4
MargotGeorget <margot.georget@etu.umontpellier.fr>
2022-02-17 22:44:52 +0100

    Added function getAllUsers : structure only, no test with url

--- Foodlab/DAO/UserDAO.swift
+++ Foodlab/DAO/UserDAO.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+struct UserDAO {
+    //TODO : mettre url en variable d'environnement 
+    var stringUrl = ""
+    
+    func getAllUsers() async -> [User]? {
+            do {
+                
+                // faire la requête vers le backend
+                guard let url = URL(string: stringUrl + "detail")
+                else { return nil }
+                let (data, _) = try await URLSession.shared.data(from: url)
+                
+                
+                // decoder le JSON avec la fonction présente dans JSONHelper
+                guard let decoded: [UserDTO] = JSONHelper.decode([UserDTO].self, data: data)
+                else { return nil }
+                
+                // dans une boucle transformer chaque UserDTO en model User
+                var users: [User] = []
+                for userDTO in decoded {
+                    users.append( getUserFromUserDTO(userDTO: userDTO))
+                }
+                
+                // retourner une liste de User
+                return users
+                
+            } catch {
+                print("Error while fetching tracks from internet: \(error)")
+                return nil
+            }
+        }
+
+    
+    func getUserFromUserDTO(userDTO : UserDTO) -> User {
+        let user = User(
+            id: userDTO.id,
+            name: userDTO.name,
+            email: userDTO.email,
+            password: userDTO.password,
+            isAdmin: userDTO.isAdmin )
+        
+        return user
+    }
+}
--- Foodlab/DTO/UserDTO.swift
+++ Foodlab/DTO/UserDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct UserDTO: Identifiable {
+struct UserDTO: Identifiable, Decodable {
     
     var id: Int?
     var name: String

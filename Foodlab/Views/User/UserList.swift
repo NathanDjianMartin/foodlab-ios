6378e9fedebe5f787e0492f6a7bd835cf9c59ba7
MargotGeorget <margot.georget@etu.umontpellier.fr>
2022-03-04 19:55:55 +0100

    Added User list view model

--- Foodlab/Views/User/UserList.swift
+++ Foodlab/Views/User/UserList.swift
@@ -1,12 +1,32 @@
 import SwiftUI
 
 struct UserList: View {
+    
     @State private var showUserCreation = false
+    @State private var userToDelete: User?
+    @ObservedObject var viewModel: UserListViewModel
+    private var intent: UserIntent
+    
+    init() {
+        self.viewModel = UserListViewModel()
+        self.intent = UserIntent()
+        self.intent.addObserver(userListViewModel: viewModel)
+    }
     
     var body: some View {
         List {
-            ForEach(MockData.usersList) { user in
+            ForEach(viewModel.users) { user in
                 UserRow(user: user)
+            }
+        }
+        .onAppear(){
+            Task {
+                switch await UserDAO.getAllUsers() {
+                case .failure(_):
+                    print("error")
+                case .success(let users):
+                    viewModel.users = users
+                }
             }
         }
         .sheet(isPresented: $showUserCreation) {
